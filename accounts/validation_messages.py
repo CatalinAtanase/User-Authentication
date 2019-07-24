7b37e86a106ec74a473a9e69edd6747ae24980ff
catalinAtanase <atanasecatalin20@gmail.com>
2019-07-24 15:53:22 +0300

    small changes

--- accounts/validation_messages.py
+++ accounts/validation_messages.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext, gettext_lazy as _
 
 class ValidationMessages:
     invalid_login = _('Numele de utilizator si parola nu se potrivesc.')
-    inactive = _('Acesc cont este inactiv') # Unused atm
+    inactive = _('Acest cont este inactiv') # Unused atm
     password_mismatch = _('Cele doua parole nu sunt identice.')
     password_incorrect = _("Parola veche a fost introdusa incorect.")
     unique_user = _('Exista deja un utilizator cu acest nume.')
